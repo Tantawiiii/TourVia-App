f4325bb362c8033c49016f81fd83cde2b8a28ae2
a7_med_01 <36699813+ahmedramadan157@users.noreply.github.com>
2019-11-25 07:16:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tourism/login/LoginActivity.java
@@ -0,0 +1,169 @@
+package com.example.tourism.login;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.tourism.MainActivity;
+import com.example.tourism.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Declaration
+    private EditText edt_LoginEmail;
+    private EditText edt_LoginPassword;
+    private Button btn_facebook;
+    private TextView tv_linkToRegister;
+
+    // FireBase Decrlation
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    // Configure Google Sign In
+    private static final int RC_SIGN_IN = 234;
+    private static final String TAG = "GoogleSignin";
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth googleAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //// initialization
+        edt_LoginEmail = findViewById(R.id.edt_loginEmail);
+        edt_LoginPassword = findViewById(R.id.edt_loginPassword);
+        Button btn_login = findViewById(R.id.btn_login);
+        btn_facebook = findViewById(R.id.btn_facebookLogin);
+        tv_linkToRegister = findViewById(R.id.tv_linkToSignUpActivity);
+
+        //Button  google LOGIN
+        Button btn_google = findViewById(R.id.btn_gmailLogin);
+        googleAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        btn_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInGoogle();
+            }
+        });
+        //Finish google LOGIN
+
+        //Action Go To Register Account
+        tv_linkToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent Reg = new Intent(LoginActivity.this , RegisterActivity.class);
+                startActivity(Reg);
+                finish();
+
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginEmail = edt_LoginEmail.getText().toString();
+                String loginPass  = edt_LoginPassword.getText().toString();
+
+                if (!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPass))
+                {
+                    mAuth.signInWithEmailAndPassword(loginEmail , loginPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful())
+                            {
+                                Intent home = new Intent(LoginActivity.this , MainActivity.class);
+                                startActivity(home);
+                                finish();
+                            }else {
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, "Error : " + errorMessage, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    // Configure Google Sign In
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (googleAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                assert account != null;
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+
+        Log.d(TAG, "fireBaseAutWithGoogle: " + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        googleAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = googleAuth.getCurrentUser();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(LoginActivity.this, "User Successfully Signed in ", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "SignIn Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // Configure Google Sign In
+}
---
+++ app/src/main/java/com/example/tourism/login/RegisterActivity.java
@@ -0,0 +1,197 @@
+package com.example.tourism.login;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.tourism.MainActivity;
+import com.example.tourism.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private CircleImageView ImageUser;
+    private EditText edt_NamwUserReg, edt_EmailUserReg, edt_PasswordUserReg;
+
+    //fireBase Declaration
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private Uri mainImageURI = null;
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+    private String uid = reference.push().getKey();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+    private String user_id = firebaseAuth.getCurrentUser().getUid();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //// initialization ::: :::
+        ImageUser = findViewById(R.id.circle_image_profile);
+        edt_EmailUserReg = findViewById(R.id.edt_RegisterEmail);
+        edt_NamwUserReg = findViewById(R.id.edt_RegisterName);
+        edt_PasswordUserReg = findViewById(R.id.edt_RegisterPassword);
+        Button btn_JoinUs = findViewById(R.id.btn_Register);
+
+        //Action Button SAVA DATA
+
+        user_id = firebaseAuth.getCurrentUser().getUid();
+        firebaseFirestore.collection("Users").document(user_id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @SuppressLint("CheckResult")
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if (task.isSuccessful()) {
+                    if (task.getResult().exists()) {
+                        String name = task.getResult().getString("name");
+                        String image = task.getResult().getString("image");
+
+                        edt_NamwUserReg.setText(name);
+
+                        RequestOptions placeholderRequest = new RequestOptions();
+                        placeholderRequest.placeholder(R.drawable.add_image);
+                        Glide.with(RegisterActivity.this).setDefaultRequestOptions(placeholderRequest).load(image).into(ImageUser);
+                    }
+
+                } else {
+                    String errorMessage = task.getException().getMessage();
+                    Toast.makeText(RegisterActivity.this, "FireStore Retrieve Error: " + errorMessage, Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        ////
+
+        btn_JoinUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String userName = edt_NamwUserReg.getText().toString();
+                String email = edt_EmailUserReg.getText().toString();
+                String password = edt_PasswordUserReg.getText().toString();
+
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(userName) && mainImageURI != null) {
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(RegisterActivity.this, "Register Successfully", Toast.LENGTH_SHORT).show();
+
+                            } else {
+                                String errorMassage = task.getException().getMessage();
+                                Toast.makeText(RegisterActivity.this, "Error : " + errorMassage, Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+                }
+
+                if (mainImageURI == null) {
+                    Toast.makeText(RegisterActivity.this, "Please select image", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                storageReference.child("profileImages").child(user_id).putFile(mainImageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            //getImageUrl
+                            storageReference.child("profileImages").child(user_id).getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+
+                                    if (task.isSuccessful()) {
+
+                                        String url = task.getResult().toString();
+                                        // irebaseFireStore Save Data
+                                        Map<String, String> userMap = new HashMap<>();
+                                        userMap.put("name", userName);
+                                        userMap.put("image", url);
+
+                                        firebaseFirestore.collection("Users").document(user_id).set(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                                if (task.isSuccessful()) {
+                                                    Intent i = new Intent(RegisterActivity.this, MainActivity.class);
+                                                    startActivity(i);
+
+                                                } else {
+                                                    String errorMessage = task.getException().getMessage();
+                                                    Toast.makeText(RegisterActivity.this, "FireStore Error: " + errorMessage, Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+
+                        } else {
+                            String errorMessage = task.getException().getMessage();
+                            Toast.makeText(RegisterActivity.this, "Error : " + errorMessage, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            }
+
+        });
+    }
+
+
+    //@Override onActivityResult method in your activity to get crop result
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                mainImageURI = result.getUri();
+                ImageUser.setImageURI(mainImageURI);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(this, "Error : " + error, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
